cb4ec385dedd2b81974d395caad4e8e05c0ab71b
jlampar <lamparskijakub@gmail.com>
2017-08-04 01:59:12 +0200

    Update cartcompjq3.js

--- cartcompjq3.js
+++ cartcompjq3.js
@@ -257,7 +257,6 @@ $(document).ready(function(){
 		console.log(typeof row);
 		console.log(presentState[row]);
 		if(presentState[row] > 0 && presentState[row] < endSwith) {
-			/*var endID = forward ? (presentState[row] + 1).join("") : (presentState[row] - 1).join("");*/
 			var endID = forward ? presentState.forEach(function(value,index) {index == row ? value + 1 : value}) : presentState.forEach(function(value,index) {index == row ? value - 1 : value});
 			console.log('endID: ',endID);
 			if(document.getElementById("0000") && document.getElementById(endID)) {
