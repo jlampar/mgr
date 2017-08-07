64c7c5513f458775763f0b3f4cec41b215a80ab9
jlampar <lamparskijakub@gmail.com>
2017-08-07 09:24:14 +0200

    Update cartcompjq3.js

--- cartcompjq3.js
+++ cartcompjq3.js
@@ -61,7 +61,7 @@
 }(document, window));
 
 $(document).ready(function(){
-	alert("Version 11.0");
+	alert("Version 12.0");
 	var h = document.getElementById("basket").offsetHeight;
 	var w = h;
 	var svg = d3.select(document.getElementById("basket"))
