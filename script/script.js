943525badbe34ac71999cde695906ff61c8174f3
jlampar <lamparskijakub@gmail.com>
2017-10-04 18:48:43 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -206,9 +206,9 @@ $(document).ready(function(){
 			
 			d3.select(document.getElementById("legendBasket")).append("div").style("display", "block").attr("id", "labels");
 			var labelDiv = d3.select(document.getElementById("labels"));
-			labelDiv.append("div").text("Odsetek widzów [%]").style("display", "inline-block").style("width", "100px").style("height", "7px").style("text-align", "right").style("margin-right", "1em").style("font-size", "small");
+			labelDiv.append("div").text("Odsetek widzów [%]").style("display", "inline-block").style("width", "100px").style("height", "7px").style("text-align", "right").style("margin-right", "1em").style("font-size", "9px");
 			labelDiv.append("div").style("width", "30px").style("height", "10px").style("display", "inline-block");
-			labelDiv.append("div").text("Liczba widzów").style("display", "inline-block").style("width", "100px").style("height", "7px").style("text-align", "right").style("margin-right", "1em").style("font-size", "small");
+			labelDiv.append("div").text("Liczba widzów").style("display", "inline-block").style("width", "100px").style("height", "7px").style("text-align", "right").style("margin-right", "1em").style("font-size", "9px");
 		
 			var commaFormat = d3.format(".2f");
 
@@ -221,14 +221,14 @@ $(document).ready(function(){
 						"powyżej " + String(commaFormat(lists.breaksRelative[l-1])) : 
 							(l == 0 ? "poniżej " + String(commaFormat(lists.breaksRelative[l+1])) : 
 							commaFormat(lists.breaksRelative[l-1]) + " - " + commaFormat(lists.breaksRelative[l]))
-					).style("display", "inline-block").style("width", "100px").style("height", "7px").style("text-align", "right").style("margin-right", "1em").style("font-size", "small");
+					).style("display", "inline-block").style("width", "100px").style("height", "7px").style("text-align", "right").style("margin-right", "1em").style("font-size", "9px");
 				liDiv.append("div").style("background-color", lists.colors[l]).style("width", "30px").style("height", "10px").style("display", "inline-block");
 				liDiv.append("div").text(
 					(l == lists.colors.length - 1) ? 
 						"powyżej " + String(lists.breaksAbsolute[l-1]) : 
 							(l == 0 ? "poniżej " + String(lists.breaksAbsolute[l+1]) : 
 							String(lists.breaksAbsolute[l-1]) + " - " + String(lists.breaksAbsolute[l]))
-					).style("display", "inline-block").style("width", "100px").style("height", "7px").style("text-align", "left").style("margin-left", "1em").style("font-size", "small");
+					).style("display", "inline-block").style("width", "100px").style("height", "7px").style("text-align", "left").style("margin-left", "1em").style("font-size", "9px");
 			}
 
 		});
